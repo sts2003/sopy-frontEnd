ea3df6b95c2f40bd83c26e75245a064f1359e60a
sts2003 <44109148+sts2003@users.noreply.github.com>
2020-12-08 21:48:33 +0900

    apollo connect

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src/components/App";
+import Client from "../src/apollo/Client";
+import { ApolloProvider } from "react-apollo-hooks";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <ApolloProvider client={Client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("app")
+);
